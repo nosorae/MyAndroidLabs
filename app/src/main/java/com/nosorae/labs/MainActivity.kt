07bfbd1d5f53fe5b6903275e762cafce6f7e1f87
nosorae <nosorae@naver.com>
2022-02-06 17:50:03 +0900

    Compose: TextField, Button, Snackbar

--- app/src/main/java/com/nosorae/labs/MainActivity.kt
+++ app/src/main/java/com/nosorae/labs/MainActivity.kt
@@ -3,36 +3,67 @@ package com.nosorae.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nosorae.labs.ui.theme.MyAndroidLabsTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyAndroidLabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            val scaffoldState = rememberScaffoldState()
+            var textFieldSate by remember {
+                // block recomposition
+                mutableStateOf("")
+            }
+            val scope = rememberCoroutineScope()
+
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        value = textFieldSate,
+                        label = {
+                            Text(text = "Enter your name")
+                        },
+                        onValueChange = { updatedString ->
+                            textFieldSate = updatedString
+
+                        },
+                        singleLine = true,
+                    )
+                    Spacer(modifier = (Modifier.height(16.dp)))
+                    Button(onClick = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldSate")
+                        }
+                    }) {
+                        Text(text = "please greet me")
+                    }
                 }
+            }
+            Snackbar {
+                Text(text = "hello snackbar")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyAndroidLabsTheme {
-        Greeting("Android")
-    }
-}
