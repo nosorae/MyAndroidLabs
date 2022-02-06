51cac988079803007c91cf247ca4c09dfa25e9f7
nosorae <nosorae@naver.com>
2022-02-06 23:23:18 +0900

    Compose: ConstraintLayout

--- app/src/main/java/com/nosorae/labs/ui/compose/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/nosorae/labs/ui/compose/ConstraintLayoutActivity.kt
@@ -3,12 +3,48 @@ package com.nosorae.labs.ui.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 class ConstraintLayoutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val constraints = ConstraintSet {
+                val greenBox = createRefFor("greenBox")
+                val redBox = createRefFor("redBox")
+                val guideline = createGuidelineFromTop(fraction = 0.5f)
 
+                constrain(greenBox) {
+                    top.linkTo(guideline)
+                    start.linkTo(parent.start)
+                    width = Dimension.percent(0.25f)
+                    height = Dimension.value(100.dp)
+                }
+                constrain(redBox) {
+                    top.linkTo(guideline)
+                    start.linkTo(greenBox.end)
+                    end.linkTo(parent.end)
+                    width = Dimension.percent(0.25f)
+                    height = Dimension.value(100.dp)
+
+                }
+                createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
+            }
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+                Box(modifier = Modifier.background(Color.Green).layoutId("greenBox"))
+                Box(modifier = Modifier.background(Color.Red).layoutId("redBox"))
+
+            }
         }
     }
 }
