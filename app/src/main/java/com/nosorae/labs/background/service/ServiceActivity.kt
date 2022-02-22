be8c767ccc450e7dfa4fc785700ccb6271b51547
nosorae <nosorae@naver.com>
2022-02-23 00:19:48 +0900

    service: dummy service test

---
+++ app/src/main/java/com/nosorae/labs/background/service/ServiceActivity.kt
@@ -0,0 +1,20 @@
+package com.nosorae.labs.background.service
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nosorae.labs.R
+
+class ServiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_service)
+
+        Intent(
+            this,
+            TimerService::class.java
+        ).also { intent ->
+            startService(intent)
+        }
+    }
+}
